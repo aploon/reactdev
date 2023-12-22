5c90b9759e67adbf2f22c1bf3b50d8f4c34a8f70
Arnaud <arnaudadjovi274@gmail.com>
2023-12-22 09:09:46 +0000

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,10 @@
 import { useState } from "react"
-import { Input } from "./components/form/Input"
-import { Checkbox } from "./components/form/Checkbox"
-import { ProduitCategorieRow } from "./components/produit/ProduitCategorieRow"
-import { ProduitRow } from "./components/produit/ProduitRow"
-
-
-const PRODUCTS = [
-    { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-    { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-    { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-    { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-    { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-    { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
-]
 
 function App() {
 
-    const [showStockedOnly, setShowStockedOnly] = useState(false)
-    const [search, setSearch] = useState('')
-
-    let produitVisible = PRODUCTS.filter((product) => {
-        
-        if(showStockedOnly) {
-            return product.stocked && product.name.toLowerCase().includes(search.toLowerCase())
-        }else{
-            return product.name.toLowerCase().includes(search.toLowerCase())
-        }
-
-    })
-
-    return <div className="Container w-25 m-auto" style={{minWidth: "350px"}}>
-        <SearchBar onShowStockedOnly={setShowStockedOnly} onSearch={setSearch}></SearchBar>
-        <ProduitTable produits={produitVisible}></ProduitTable>
-    </div>
-}
-
-
-function SearchBar({onShowStockedOnly, onSearch}) {
-
-    return <div className="mt-3">
-        <Input placeholder="Rechercher..." onChange={onSearch}></Input>
-        <Checkbox id="formCheckbox" onChange={onShowStockedOnly}></Checkbox>
-    </div>
-
-}
-
-function ProduitTable({produits}) {
-
-    let rows = []
-    let lastCategorie = ""
-
-    for (let produit of produits) {
-
-        if (produit.category != lastCategorie) {
-            rows.push(<ProduitCategorieRow key={produit.category} categorie={produit.category}></ProduitCategorieRow>)
-        }
-
-        rows.push(<ProduitRow key={produit.name} produit={produit}></ProduitRow>)
-
-        lastCategorie = produit.category
-    }
-
-    return <div>
-        <table className="table">
-            <thead>
-                <tr>
-                    <th>Nom</th>
-                    <th>Prix</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                {rows}
-            </tbody>
-        </table>
-    </div>
-
+    // something
+    
+    return ""
 }
 
 export default App
--- src/TodoList.jsx
+++
@@ -1,9 +0,0 @@
-function TodoListElement({todoList}) {
-
-    return <ul>
-        {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
-      </ul>
-  
-}
-
-export default TodoListElement
