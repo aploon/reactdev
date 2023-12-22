3cab81b42ee7f48d2415f0411bb4808ff917a03a
Arnaud <arnaudadjovi274@gmail.com>
2023-12-22 11:24:06 +0000

    Compteur TP

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react"
 
 function App() {
 
+    // state (etat, donnees)
+    [count, setCount] = useState(0)
+
+    // comportements
+    handleClick = (e) => {
+        setCount(count + 1)
+    }
+
+    // affichage (render)
     return <>
-        <h1>Hello world</h1>
+        <div className="container mt-2 m-auto" style={{minWith: "350px"}}>
+            <h1>Compteur</h1>
+            <p>{count}</p>
+            <button onClick={handleClick}>Click</button>
+        </div>
     </>
 }
 
