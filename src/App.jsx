3f863135ee106d88b08ad2d13f2b90125fb15f86
Arnaud <arnaudadjovi274@gmail.com>
2024-01-16 09:54:56 +0000

    Compteur automatique

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(3)
 
-  useEffect(() => {
-    // Démarre un timer qui incrémente le compteur toutes les 1000 millisecondes (1 seconde)
-    const timer = setInterval(() => {
-      setCount(prevCount => prevCount + 1);
+  let autoCount = () => {
+
+    timer =  setInterval(() => {
+        setCount(v => v - 1)
+        if(count == 0){
+            clearInterval(timer) 
+        }
     }, 1000);
-
-    // Nettoie le timer lorsque le composant est démonté
-    return () => clearInterval(timer);
-  }, [count]); // Le tableau vide [] indique que cet effet ne dépend d'aucune variable d'état ou de propriété
+    
+  }
 
   return (
     <div>
-        <input type="number" onChange={e => setCount(Number(e.target.value))}/>
-        <h1>Compteur automatique : {count}</h1>
+        <input type="number" onChange={e => {setCount()}}/>
+        <h1>Compteur automatique : {count}</h1> 
     </div>
   );
 }
